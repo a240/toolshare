54b25d3c972fe2e3a3f3f7a09941b430709f69aa
David Samuelson <davidhlemon@gmail.com>
2014-03-04 21:46:01 -0500

    Added Share Contracts
    Reorganized the models adding share contracts

--- sharetools/admin.py
+++ sharetools/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from sharetools.models import Shed
+from sharetools.models import Shed, Tool, UserProfile, ShareContract
 
 # Register your models here.
 
 admin.site.register(Shed)
+admin.site.register(Tool)
+admin.site.register(UserProfile)
+admin.site.register(ShareContract)
--- sharetools/models.py
+++ sharetools/models.py
@@ -3,40 +3,74 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	zipcode = models.CharField(max_length=5)
+
+	def __str__(self):
+		return self.user.username + '\'s ' + self.zipcode
+
+
 class Shed(models.Model):
+	owner = models.OneToOneField(User)
+	name = models.CharField(max_length=80)
 	zipcode = models.IntegerField(default=0)
-	name = models.CharField(max_length=80, unique=True)
-	isCommunity = models.BooleanField(editable=False)
+	isCommunity = models.BooleanField(default=True)
 	isActive = models.BooleanField(default=True)
 	isPrivate = models.BooleanField(default=False)
 	dateCreated = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-		return name + " | " + zipcode + " | " + owner
+		return self.owner.username + "'s " + self.name
 
-
-class UserToolShareProfile(models.Model):
-	user = models.OneToOneField(User, unique=True)
-	shed = models.OneToOneField(Shed)
-	zipcode = models.IntegerField()
-
-	def __str__(self):
-		return user.username + ' | ' + zipcode
 	
 class Tool(models.Model):
-	name = models.CharField(max_length=80)
-	description = models.CharField(max_length=300)
-	owner = models.OneToOneField(UserToolShareProfile, unique=True)	
-	type = models.CharField(max_length=80)
+	TOOL_CHOICES = (
+			('Drills', (
+					('hammerDrill', 'Hammer Drill'),
+					('pressDrill', 'Press Drill'),
+					('airDrill', 'Air Drill'),
+					('percussionDrill', 'Percussion Drill'),
+					('powerDirll', 'Power Drill'),
+				)
+			),
+			('Screwdriver', (
+					('slottedScrewdriver', 'Slotted Screwdriver'),
+					('flaredScrewdriver', 'Flared Screwdriver'),
+				)
+			),
+			('Wrenches', (
+					('torqueWrench', 'Torque Wrench'),
+					('pipeWrench', 'Pipe Wrench'),
+					('strapWrench', 'Strap Wrench'),
+				)
+			),
+			('Hammer', (
+					('clawHammer', 'Claw Hammer'),
+					('powerHammer', 'Power Hammer'),
+				)
+			)
+		)
+
+	currentShed = models.OneToOneField(Shed)
+	type = models.CharField(max_length=80, choices=TOOL_CHOICES)
+	description = models.CharField(max_length=300, blank=True)
+	owner = models.OneToOneField(User)
 	available =  models.BooleanField()
-	status = models.CharField(max_length=300)
-	borrower = models.OneToOneField(UserToolShareProfile, related_name="borrowing_user")
-	currShed = models.OneToOneField(Shed)
-	expectedAvailabilityDate = models.DateTimeField()
-	dateLoaned = models.DateTimeField()
 
 	def __str__(self):
-		return 
+		return self.owner.username + '\'s ' + self.type
+
+class ShareContract(models.Model):
+	returnDate = models.DateTimeField()
+	loanDate = models.DateTimeField()
+	lender = models.OneToOneField(User, related_name='lender')
+	borrower = models.OneToOneField(User, related_name='borrower')
+	tool = models.OneToOneField(Tool, related_name='tool')
+
+	def __str__(self):
+		return self.lender + ' lent ' + self.borrower + ' on ' + loanDate
+
 
 def makeTool(owner, name, description, type):
 	tool = Tool()
