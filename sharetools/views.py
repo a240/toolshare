7069a56c91f8393f8520e597113bec89b2311784
buzz66boy <mda5893@rit.edu>
2014-03-08 20:30:53 -0500

    registration can error now

--- sharetools/views.py
+++ sharetools/views.py
@@ -2,144 +2,155 @@ import hashlib
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from django.contrib.auth.hashers import make_password
 
 from sharetools.models import Asset, Location, UserProfile, User
-from sharetools.forms import LoginForm, UserForm, UserEditForm 
+from sharetools.forms import LoginForm, UserForm, UserEditForm
+
 
 def index_view(request):
-	if request.user.is_authenticated():
-		template = loader.get_template('base_index.html')
-		context = RequestContext(request, {
-		})
-		return HttpResponse(template.render(context))
-	else:
-		template = loader.get_template('landing.html')
-		context = RequestContext(request, {
-		})
-		return HttpResponse(template.render(context))
+    if request.user.is_authenticated():
+        template = loader.get_template('base_index.html')
+        context = RequestContext(request, {
+        })
+        return HttpResponse(template.render(context))
+    else:
+        template = loader.get_template('landing.html')
+        context = RequestContext(request, {
+        })
+        return HttpResponse(template.render(context))
+
 
 def login_view(request):
-	if request.user.is_authenticated():
-		return redirect('index')
-	else:
-		if request.method == 'POST':
-			form = LoginForm(request.POST)
-			if form.is_valid():
-				username = form.cleaned_data['username']
-				password = form.cleaned_data['password']
-				user = authenticate(username=username, password=password)
-				if user is not None and user.is_active:
-					login(request, user)
-					return redirect('index')
+    if request.user.is_authenticated():
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(username=username, password=password)
+                if user is not None and user.is_active:
+                    login(request, user)
+                    return redirect('index')
 
-			context = RequestContext(request, {
-				'login_error': "The username and email you gave us did not match up"
-			})
-		else:
-			context = RequestContext(request, {})
+            context = RequestContext(request, {
+                'login_error': "The username and email you gave us did not match up"
+            })
+        else:
+            context = RequestContext(request, {})
 
-		template = loader.get_template('base_login.html')
-		return HttpResponse(template.render(context))
+        template = loader.get_template('base_login.html')
+        return HttpResponse(template.render(context))
+
 
 def logout_view(request):
-	logout(request)
-	return redirect('index')
+    logout(request)
+    return redirect('index')
+
 
 def register_view(request):
-	if request.user.is_authenticated():
-		return redirect('index')
-	else:
-		if request.method == 'POST':
-			user_form = UserForm(request.POST)
-			# @TODO Check if the user already exist and post good errors
-			if user_form.is_valid():
-				user = user_form.save()
-				# if User.objects.filter(username=user.username, email.user.email).exists():
-				profile = UserProfile()
-				profile.user = user
-				profile.save()
-				return redirect('login')
+    if request.user.is_authenticated():
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            user_form = UserForm(request.POST)
+            if user_form.is_valid():
+                user = user_form.save()
+                profile = UserProfile()
+                profile.user = user
+                profile.save()
+                return redirect('login')
+            else:
+                messages.add_message(request, messages.WARNING, 'Form Submission Error.', extra_tags='alert-warning')
+                return redirect('register')
+        context = RequestContext(request, {
+            'form': UserForm()
+        })
+        template = loader.get_template('base_register.html')
+        return HttpResponse(template.render(context))
 
-		context = RequestContext(request, {
-			'form': UserForm()
-		})
-		template = loader.get_template('base_register.html')
-		return HttpResponse(template.render(context))
 
 def my_profile_view(request):
-	return profile_view(request, request.user.username)
+    return profile_view(request, request.user.username)
+
 
 # Lets anyone view the profile/username/ user's info
 def profile_view(request, user_id):
-	this_user = get_object_or_404(User, username__iexact=user_id)
-	user_profile = this_user.userprofile
-	m = hashlib.md5()
-	m.update(request.user.email)
-	hashedEmail = m.digest()
-	avatarURL = 'http://www.gravatar.com/avatar/' + hashedEmail + '?d=identicon'
-	template = loader.get_template('base_profile.html')
-	context = RequestContext(request, {
-		'userProfile': user_profile,
-		'avatarURL': hashedEmail,
-	})
-	
-	return HttpResponse(template.render(context))
+    this_user = get_object_or_404(User, username__iexact=user_id)
+    user_profile = this_user.userprofile
+    m = hashlib.md5()
+    m.update(request.user.email)
+    hashedEmail = m.digest()
+    avatarURL = 'http://www.gravatar.com/avatar/' + hashedEmail + '?d=identicon'
+    template = loader.get_template('base_profile.html')
+    context = RequestContext(request, {
+        'userProfile': user_profile,
+        'avatarURL': hashedEmail,
+    })
+
+    return HttpResponse(template.render(context))
+
 
 # Allows users to modify their profile
 # Uses UserEditForm, redirects user to their profile view upon success
 # @Phil
 def edit_profile_view(request):
-	if request.method == 'POST':
-		form = UserEditForm(request.POST, instance=request.user)
-		if form.is_valid():
-			request.user.userprofile.zipcode = form.cleaned_data['zipcode']
-			request.user.password = make_password(form.cleaned_data['password'],'pbkdf2_sha256')
-			request.user.userprofile.save()
-			form.save()
-			return HttpResponseRedirect('/profile/'+request.user.username)
-	else:
-		form = UserEditForm(initial = {
-			'first_name': request.user.first_name,
-			'last_name': request.user.last_name,
-			'zipcode': request.user.userprofile.zipcode,
-			'email': request.user.email
-		})
-		
-	return render(request, 'base_editProfile.html', {
-		'form':form,
-	})
+    if request.method == 'POST':
+        form = UserEditForm(request.POST, instance=request.user)
+        if form.is_valid():
+            request.user.userprofile.zipcode = form.cleaned_data['zipcode']
+            request.user.password = make_password(form.cleaned_data['password'], 'pbkdf2_sha256')
+            request.user.userprofile.save()
+            form.save()
+            return HttpResponseRedirect('/profile/' + request.user.username)
+    else:
+        form = UserEditForm(initial={
+            'first_name': request.user.first_name,
+            'last_name': request.user.last_name,
+            'zipcode': request.user.userprofile.zipcode,
+            'email': request.user.email
+        })
+
+    return render(request, 'base_editProfile.html', {
+        'form': form,
+    })
+
 
 def shed_view(request, shed_id):
-	shedLocation = get_object_or_404(Location, pk=shed_id)
-	assets = Asset.objects.filter(location=shedLocation)
-	template = loader.get_template('base_shed.html')
-	context = RequestContext(request, {
-		'location': shedLocation,
-		'assets': assets,
-	})
-	return HttpResponse(template.render(context))
+    shedLocation = get_object_or_404(Location, pk=shed_id)
+    assets = Asset.objects.filter(location=shedLocation)
+    template = loader.get_template('base_shed.html')
+    context = RequestContext(request, {
+        'location': shedLocation,
+        'assets': assets,
+    })
+    return HttpResponse(template.render(context))
+
 
 def my_sheds_view(request):
-	shedLocations = Location.objects.filter(owner=request.user)
-	template = loader.get_template('base_mySheds.html')
-	context = RequestContext(request, {
-		'shedLocations': shedLocations,
-	})
-	return HttpResponse(template.render(context))
+    shedLocations = Location.objects.filter(owner=request.user)
+    template = loader.get_template('base_mySheds.html')
+    context = RequestContext(request, {
+        'shedLocations': shedLocations,
+    })
+    return HttpResponse(template.render(context))
 
 
 def my_tools_view(request):
-	assets = Asset.objects.filter(owner=request.user)
-	template = loader.get_template('base_myTools.html')
-	context = RequestContext(request, {
-		'assets': assets,
-	})
-	return HttpResponse(template.render(context))
+    assets = Asset.objects.filter(owner=request.user)
+    template = loader.get_template('base_myTools.html')
+    context = RequestContext(request, {
+        'assets': assets,
+    })
+    return HttpResponse(template.render(context))
+
 
 def tool_view(request, id):
-	return HttpResponse("Tool page." + id)
+    return HttpResponse("Tool page." + id)
 
