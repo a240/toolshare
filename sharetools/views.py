6714e9294e119db92d85b807f5649a20a7508fba
briggsotis <pgl5711@rit.edu>
2014-03-10 11:28:29 -0400

    Added Tool Page
    Users can view their individual tools

--- sharetools/views.py
+++ sharetools/views.py
@@ -174,7 +174,8 @@ def shed_delete_view(request, shed_id):
 	else:
 		messages.add_message(request, messages.WARNING, 'You do not have that permission.', extra_tags='alert-warning')
 	return redirect('mySheds')
-
+    
+	
 def my_tools_view(request):
 	assets = Asset.objects.filter(owner=request.user)
 	template = loader.get_template('base_myTools.html')
@@ -203,9 +204,22 @@ def make_tool_view(request):
 	})
 
 
-def tool_view(request, vid):
-	return HttpResponse("Tool page." + vid)
+def tool_view(request, tool_id):
+	asset = get_object_or_404(Asset, pk=tool_id)
 
+	context = RequestContext(request, {
+		'user': request.user,
+		'asset': asset
+	})
+	
+	template = loader.get_template('base_tool.html')
+	return(HttpResponse(template.render(context)))
+
+def tool_delete_view(request, tool_id):
+	return(HttpResponse('Tool delete ' + tool_id))
+	
+def tool_edit_view(request, tool_id):
+	return(HttpResponse('Tool edit ' + tool_id))
 
 def messages_view(request):
 	template = loader.get_template('base_messages_inbox.html')
