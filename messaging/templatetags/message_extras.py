1e5fd481ffee8b0dee05ef27ede2d107c9eade05
David Samuelson <davidhlemon@gmail.com>
2014-03-12 17:57:42 -0400

    Fixed for messaging tag

--- messaging/templatetags/message_extras.py
+++ messaging/templatetags/message_extras.py
@@ -8,11 +8,4 @@ def unread_message_count(user):
 	count = Message.objects.filter(msg_to=user, read=False).count()
 	return count
 
-def has_unread_messages(user):
-	if (unread_message_count(user) > 0):
-		return True
-	else:
-		return False
-
 register.simple_tag(unread_message_count)
-register.simple_tag(has_unread_messages)
