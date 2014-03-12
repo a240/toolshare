4bfda658b7fc5496b70a0a13383e8ab1489d8778
buzz66boy <battlefieldgeek@aim.com>
2014-03-11 21:53:06 -0400

    can send messages now

--- messaging/forms.py
+++ messaging/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from messaging.models import Message
 
 class MessageForm(forms.ModelForm):
+	to = forms.CharField()
+
 	class Meta:
 		model = Message
-		fields = ('msg_to','subject','body')
+		fields = ('subject','body')
--- messaging/views.py
+++ messaging/views.py
@@ -2,13 +2,40 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from messaging.models import Message
+from messaging.forms import MessageForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from messaging.forms import MessageForm
 
 def messages_view(request):
+	if not request.user.is_authenticated():
+		return redirect('landing')
+	if request.method == 'POST':
+		form = MessageForm(data=request.POST)
+		print(request.POST)
+		if form.is_valid():
+			print('hey')
+			subject = form.cleaned_data['subject']
+			to = form.cleaned_data['to']
+			body = form.cleaned_data['body']
+			send_to = User.objects.filter(username=to)
+			if send_to.count() != 0:
+				msg = Message()
+				msg.msg_from = request.user
+				msg.msg_to = send_to[0]
+				msg.body = body
+				msg.subject = subject
+				msg.save()
+				messages.add_message(request, messages.SUCCESS, 'Message sent Successfully', extra_tags='alert-success')
+			else:
+				messages.add_message(request, messages.WARNING, 'User Does Not Exist.', extra_tags='alert-danger')
+		return redirect('messages')
 	template = loader.get_template('base_messages_inbox.html')
-	messages = Message.objects.filter(msg_to=request.user)
-	if messages.count() != 0:
-		args = {'user_messages': messages}
-		        #'form': MessageForm}
+	message_list = Message.objects.filter(msg_to=request.user)
+	if message_list.count() != 0:
+		args = {'user_messages': message_list,
+		        'form': MessageForm}
 	else:
 		args = {}
 	context = RequestContext(request, args)
