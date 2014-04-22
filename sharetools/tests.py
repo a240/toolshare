923727f9a5606f7d7335b4821fe518c9088f3f69
Matthew J. Anderson <leaviathan@gmail.com>
2014-04-22 12:38:36 -0400

    Cleaned up junk tests.

--- sharetools/tests.py
+++ sharetools/tests.py
@@ -3,52 +3,7 @@ import unittest
 from sharetools.forms import *
 
 # Create your tests here.
-"""
-class Tests1(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/templates/base_login/", {'something':'something'})
-		self.assertFormError(response, 'form', 'something', 'This field is required.')
 
-class Tests2(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/templates/base_makeShare/", {'something':'something'})
-		self.assertFormError(response, 'form', 'something', 'This field is required.')
-
-class Tests3(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/templates/base_makeTool", {'something':'something'})
-		self.assertFormError(response, 'form', 'something', 'This field is required.')
-
-class Tests4(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/templates/base_editProfile/", {'something':'something'})
-		self.assertFormError(response, 'form', 'something', 'This field is required.')
-
-class Tests5(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/forms.py", {'something':'something'})
-		self.assertFormError(response, 'form', 'UserEditForm', 'This field is required.')
-
-class Tests6(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/forms.py", {'something':'something'})
-		self.assertFormError(response, 'form', 'something', 'This field is required.')
-
-class Tests7(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/forms.py", {'something':'something'})
-		self.assertFormError(response, 'form', 'something', 'This field is required.')
-
-class Tests8(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/views.py", {'something':'something'})
-		self.assertFormError(response, 'form', 'something', 'This field is required.')
-
-class Tests9(TestCase):
-	def test_forms(self):
-		response = self.client.post("/sharetools/views.py", {'MakeToolForm':'email'})
-		self.assertFormError(response, 'form', 'MakeToolForm', 'This field is required.')
-"""
 
 class Formtests(unittest.TestCase):
 	def test_validation(self):
@@ -71,7 +26,7 @@ class FormTests2(unittest.TestCase):
 class FormTests3(unittest.TestCase):
 	def test_validation(self):
 		form_data = {
-			'name': 'x' * 300,
+			'loandate': datetime.datetime.today()
 		}
 
 		form = MakeShareForm(data=form_data)
@@ -80,24 +35,27 @@ class FormTests3(unittest.TestCase):
 class FormTests4(unittest.TestCase):
 	def test_validation(self):
 		form_data = {
-			'loandate': 'x' * 300,
+			'user': 'testuser',
 		}
 
 		form = MakeToolForm(data=form_data)
 		self.assertFalse(form.is_valid())
 
-"""
-class Tests10(unittest.TestCase):
-	def setUp(self):
-		self.User1 = UserForm.email
-		self.User2 = UserForm.email
+class FormTests5(unittest.TestCase):
+	def test_validation(self):
+		form_data = {
+			'name': 'x' * 300,
+		}
 
-class Tests11(unittest.TestCase):
-	def setUp(self):
-		a = ['larry', 'curly', 'moe']
-		self.assertEqual(UserForm(a, 0), 'larry')
-		self.assertEqual(UserForm(a, 1), 'curly')
-"""
+		form = AssetSearchForm(data=form_data)
+		self.assertFalse(form.is_valid())
+
+#class FormTests6(unittest.TestCase):
+#	def test_validation(self):
+#		form_data = {
+#			''
+#		}
+
 
 #class Tests12(unittest.TestCase):
 #	def setUp(self):
